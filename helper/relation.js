07da4f7348090adac7bbdad827a220e4a7bef016
Lenovo <nashirresta7@gmail.com>
2022-03-23 21:20:38 +0700

    ntahlah

--- helper/relation.js
+++ helper/relation.js
@@ -3,6 +3,6 @@ const { DataTypes } = require("sequelize");
 const User = require("../models/user")(sequelize, DataTypes);
 const Notes = require("../models/notes")(sequelize, DataTypes);
 
-User.hasMany(Notes, { foreignKey: "userId" });
+// User.hasMany(Notes, { foreignKey: "userId" });
 
 module.exports = { User, Notes };
